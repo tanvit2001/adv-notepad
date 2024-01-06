f72035e9d7ab727b235480bf7c4ce5f244813c6d
tanvit2001 <tanvitanksale19@gmail.com>
2024-01-06 09:59:19 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './component/NavBar';
+import Notepad from './component/Notepad';
+
+function App() {
+  return (
+    <div>
+      <>
+      <NavBar/>
+      <Notepad/>
+      </>
+    </div>
+  );
+}
+
+export default App;
